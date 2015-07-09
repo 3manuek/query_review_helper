2df62c866ad2253b4211f1f068840ca959e2851c
Emanuel <3manuek@esdebian.org>
2015-07-09 12:01:49 -0300

    Create download_slow_log_RDS.sql

---
+++ download_slow_log_RDS.sql
@@ -0,0 +1,8 @@
+-- Download the slow_log table into a file
+-- http://www.pythian.com/blog/exporting-the-mysql-slow_log-table-into-slow-query-log-format/
+
+mysql -u user -p -h host.rds.amazonaws.com -D mysql -s -r -e "SELECT \
+CONCAT( '# Time: ', DATE_FORMAT(start_time, '%y%m%d %H%i%s'), '\n', \
+'# User@Host: ', user_host, '\n', '# Query_time: ', TIME_TO_SEC(query_time),  \
+'  Lock_time: ', TIME_TO_SEC(lock_time), '  Rows_sent: ', rows_sent, '  Rows_examined: ', \
+rows_examined, '\n', sql_text, ';' ) FROM mysql.slow_log" > /tmp/mysql.slow_log.log
