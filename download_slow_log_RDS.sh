1ea1708aaae010ca925aeaddcee42914934c0a67
Emanuel <3manuek@esdebian.org>
2015-07-27 15:54:11 -0300

    Update download_slow_log_RDS.sh

--- download_slow_log_RDS.sh
+++ download_slow_log_RDS.sh
@@ -3,9 +3,10 @@
 
 hostname=$1
 username=$2
+outputfile=/tmp/mysql.slow_log.log
 
 mysql -u $username -p -h $hostname -D mysql -s -r -e "SELECT \
 CONCAT( '# Time: ', DATE_FORMAT(start_time, '%y%m%d %H%i%s'), '\n', \
 '# User@Host: ', user_host, '\n', '# Query_time: ', TIME_TO_SEC(query_time),  \
 '  Lock_time: ', TIME_TO_SEC(lock_time), '  Rows_sent: ', rows_sent, '  Rows_examined: ', \
-rows_examined, '\n', sql_text, ';' ) FROM mysql.slow_log" > /tmp/mysql.slow_log.log
+rows_examined, '\n', sql_text, ';' ) FROM mysql.slow_log" > $outputfile
